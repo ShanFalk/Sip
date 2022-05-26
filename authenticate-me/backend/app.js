2392675b5d54b35a4fe76a18fbfce395c498c624
Shannon Falk <falk.shannon@gmail.com>
2022-05-26 15:56:01 -0400

    fixed csrftoken func bug by moving routes to bottom of app file

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -14,7 +14,6 @@ const app = express();
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
-app.use(routes);
 
 if (!isProduction) {
     // enable cors only in development
@@ -36,5 +35,7 @@ app.use(
       }
     })
   );
+
+app.use(routes);
 
 module.exports = app;
