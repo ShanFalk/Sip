17c1e449b12fac4aa29464b28e7e047dba8257b6
Shannon Falk <falk.shannon@gmail.com>
2022-05-27 13:54:46 -0400

    validation for user login works!

--- authenticate-me/backend/routes/api/session.js
+++ authenticate-me/backend/routes/api/session.js
@@ -6,9 +6,20 @@ const { User } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
+
 
 //User login route
-router.post('/', asyncHandler(async(req, res, next) => {
+router.post('/', validateLogin, asyncHandler(async(req, res, next) => {
         const { credential, password } = req.body;
         const user = await User.login({credential, password});
 
