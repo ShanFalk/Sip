72f342451be911107755919983236e02124db044
Shannon Falk <falk.shannon@gmail.com>
2022-05-26 20:19:18 -0400

    tested auth middleware response is good

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, restoreUser } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
 router.post('/test', function(req, res) {
     res.json({ requestBody: req.body})
-})
+});
+
+
 
 
 module.exports = router;
