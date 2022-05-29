6b983f112741636b97a7897bda6951dc90c2a1da
Shannon Falk <falk.shannon@gmail.com>
2022-05-29 15:58:58 -0400

    created signup middleware thunk, component, and css

--- authenticate-me/frontend/src/App.js
+++ authenticate-me/frontend/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from './components/SignupFormPage';
 import { restoreUser } from './store/session';
 import { useDispatch } from 'react-redux';
 
@@ -18,6 +19,9 @@ function App() {
     <Switch>
       <Route path='/login'>
         <LoginFormPage />
+      </Route>
+      <Route path='/signup'>
+        <SignupFormPage />
       </Route>
     </Switch>
     </>
