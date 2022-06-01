2490999a3eeff6f46de7fa8422db230e86581a0e
Shannon Falk <falk.shannon@gmail.com>
2022-06-01 11:52:55 -0400

    created business reducer action creator and thunk

---
+++ authenticate-me/frontend/src/store/business.js
@@ -0,0 +1,39 @@
+import { csrfFetch } from "./csrf";
+
+const ADD_BUSINESS = 'business/ADD_BUSINESS';
+const DELETE_BUSINESS = 'business/DELETE_BUSINESS';
+const LOAD_BUSINESSES = 'business/LOAD_BUSINESSES';
+const UPDATE_BUSINESS = 'business/UPDATE_BUSINESS';
+
+const addBusiness = (business) => {
+    return {
+        type: ADD_BUSINESS,
+        business
+    }
+}
+
+export const createBusiness = (business) => async (dispatch) => {
+
+    const res = await csrfFetch('/api/businesses', {
+        method: 'POST',
+        body: JSON.stringify(business)
+    })
+    if (res.ok) {
+        const data = await res.json();
+        dispatch(addBusiness(data.business));
+    }
+}
+
+
+const initialState = { business: null }
+
+const businessReducer = (state = initialState, action) => {
+    Object.freeze(state);
+    let newState;
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+
+export default businessReducer;
