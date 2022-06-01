7655e326527a1e995c17f4b41813c106d4c27837
Shannon Falk <falk.shannon@gmail.com>
2022-06-01 16:46:57 -0400

    added validators to post business route

--- authenticate-me/backend/routes/api/businesses.js
+++ authenticate-me/backend/routes/api/businesses.js
@@ -7,7 +7,46 @@ const { Business } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
-router.post('/', requireAuth, asyncHandler(async(req, res) => {
+const validateBizCreate = [
+    check('ownerId')
+        .exists({ checkFalsy: true }),
+    check('title')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a title')
+        .isLength({max: 256})
+        .withMessage('Title must not be longer than 256 characters'),
+    check('description')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a description'),
+    check('address')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide an address')
+        .isLength({max: 256})
+        .withMessage('Address must not be longer than 256 characters'),
+    check('city')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a city')
+        .isLength({max: 256})
+        .withMessage('City must not be longer than 256 characters'),
+    check('state')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a state')
+        .isLength({max: 256})
+        .withMessage('State must not be longer than 256 characters'),
+    check('zipCode')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a zipCode')
+        .isLength({max: 50})
+        .withMessage('Zip code must not be longer than 50 characters'),
+    check('imageUrl')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide an image URL')
+        .isLength({max: 256})
+        .withMessage('Image URL must not be longer than 256 characters'),
+    handleValidationErrors
+];
+
+router.post('/', requireAuth, validateBizCreate, asyncHandler(async(req, res) => {
 
     const {
         ownerId,
