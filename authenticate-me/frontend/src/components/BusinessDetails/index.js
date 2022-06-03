9b85c2246a17a9c9ab428c6df86d3f72af2839f4
Shannon Falk <falk.shannon@gmail.com>
2022-06-03 10:41:22 -0400

    commit to wrong branch

--- authenticate-me/frontend/src/components/BusinessDetails/index.js
+++ authenticate-me/frontend/src/components/BusinessDetails/index.js
@@ -1,21 +1,41 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { readBusiness } from "../../store/business";
+import Navigation from "../Navigation";
 import Business from "./Business";
 import UpdateBusiness from "./UpdateBusiness";
 
 const BusinessDetails = () => {
+
+    const { businessId } = useParams();
+
+    const dispatch = useDispatch();
+    const business = useSelector(state => state.businessState.businesses);
+    const user = useSelector(state => state.sessionState.user);
+
     const [isEditing, setIsEditing] = useState(false);
 
     const onEditEnd = () => (setIsEditing(false));
 
     const onEditStart = () => (setIsEditing(true));
 
+    useEffect(() => {
+
+        dispatch(readBusiness(businessId));
+
+    }, [dispatch]);
+
 
     return (
         <>
+        <Navigation />
         {isEditing && (
-            <UpdateBusiness />
+            <UpdateBusiness props={business}/>
         )}
-        <Business />
+        <Business business={business}/>
         </>
     )
 }
+
+export default BusinessDetails;
