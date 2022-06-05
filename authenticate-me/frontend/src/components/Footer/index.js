b187120f509b02025b2370600b19e78f8ec714c8
Shannon Falk <falk.shannon@gmail.com>
2022-06-04 20:07:12 -0400

    fixing footer

--- authenticate-me/frontend/src/components/Footer/index.js
+++ authenticate-me/frontend/src/components/Footer/index.js
@@ -2,7 +2,15 @@ import './Footer.css'
 
 const Footer = () => {
     return (
-        <footer></footer>
+        <footer>
+            <div className='footer-div'>
+                <ul className='footer-ul'>
+                    <li>Created By: Shannon Falk</li>
+                    <li>Github</li>
+                    <li>LinkedIn</li>
+                </ul>
+            </div>
+        </footer>
     )
 }
 
